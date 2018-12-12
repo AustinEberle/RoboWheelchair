8c03b9378381985c8a7e37ca4db9fd5c871887cd
Amulek1416 <zcarico03@gmail.com>
2018-12-12 13:17:46 -0700

    Updated README and Schematic in RoboWheelChair.ino Added 3D modeled parts

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # RoboWheelchair
-Go to https://github.com/jaean123/ArduinoBlue-library/releases to find ArduinoBlue bluetooth library
+
+MATERIALS:
+   - 1 Arduino Nano
+   - 2 Electric Motors
+   - 1 Motor Drivers
+   - 2 Caster Wheels
+   - 2 Motor to Wheel Adapters (custom made for motors, file 
+         provided under parts folder)
+
+Special Thanks to ARDUINOBLUE for providing app and Libraries.
+
+Go to https://github.com/jaean123/ArduinoBlue-library/releases to find
+ ArduinoBlue bluetooth library.
+
+For information on ARDUINOBLUE app, go to
+ https://sites.google.com/stonybrook.edu/arduinoble/documentation
+
+
+
--- RoboWheelchair.ino
+++ RoboWheelchair.ino
@@ -5,34 +5,47 @@
  * 
  * CIRCUIT:
  *                  ____________________
- *     ________     | MOTOR Board       |     ________ 
+ *     ________     |                   |     ________ 
  *    |        |____|A1               B1|____|        |
  *    | MOTORA |____|A2               B2|____| MOTORB |
  *    |________|    |                   |    |________|
- *                  |12v GND 5v   eA  eB|
+ *                  |       MOTOR       |
+ *                  |       BOARD    nA1|____(to arduino digital pin)
+ *                  |                nA2|____(to arduino digital pin)
+ *                  |                nB1|____(to arduino digital pin)
+ *                  |                nB2|____(to arduino digital pin)
+ *                  |12v GND  5v  eA  eB|
  *                  ---------------------
  *    _____________   |   |   |   |   |
  *   | 12v Power   |  |   |   |   |   |
- *   | Supply (PWR)|__|   |   |   |   L__(to arduino PMW pin)
- *   |        (GND)|------+   |   L______(to another arduino PMW pin)  
- *   ---------------      |   L__________(to arduino Vin pin)
- *                        +--------------(to arduino Gnd pin)
+ *   | Supply (PWR)|__|   |   |   |   L______(to arduino PMW pin)
+ *   |        (GND)|------+   |   L__________(to another arduino PMW pin)  
+ *   ---------------      |   L______________(to arduino Vin pin)
+ *                        +------------------(to arduino Gnd pin)
  *     _______________    |
  *    | BLUETOOTH     |   |
  *    | Pwr Tx Rx GND |   |
  *    -----------------   |
  *       |  |  |   |      |
- *       |  |  |   L______+-(2kOhm)-+-(1kOhm)-(to arduino TX)
+ *       |  |  |   L------+-(2kOhm)-+-(1kOhm)-(to arduino TX)
  *       |  |  L____________________|
- *       |  |_____________________________(to arduino RX)
- *       |________________________________(to arduino Vout)
+ *       |  |_________________________________(to arduino RX)
+ *       |____________________________________(to arduino Vout)
+ * 
+ * SUMMARY: Uses the iPhone app "ArduinoBlue" to connect to a wheelchair
+ *          via bluetooth 
+ * 
+ * CITATIONS: Special thanks to ARDUINOBLUE for providing the app and
+ *            libraries to make this project work smoothly (See README
+ *            for links to their website).
+ * 
  * *********************************************************************/
 
 ///LIBRARIES////////////////
-#include <SoftwareSerial.h>
-#include <ArduinoBlue.h>
-#include <avr/sleep.h>
-#include "motor.h"
+#include <SoftwareSerial.h> //For debugging
+#include <ArduinoBlue.h>    //Handle connecting chair to phone
+#include <avr/sleep.h>      //Put arduino to sleep
+#include "motor.h"          
 #include "chair.h"
 ///////////////////////////
 
@@ -49,11 +62,11 @@ int throttle, steering = 0;
 // set bluetooth TX and RX pins
 const int BLUETOOTH_TX = 3;
 const int BLUETOOTH_RX = 2;
-///////////////////////////
 
 //create an instance of an object that will talk to the arduino
 SoftwareSerial bluetooth(BLUETOOTH_TX, BLUETOOTH_RX);
 ArduinoBlue phone(bluetooth);
+///////////////////////////
 
 ///PROTOTYPES///////////////////////////
 void moveChair();
@@ -83,23 +96,44 @@ void setup()
 ///SETUP INTERUPTS/////////////////////
 //  attachInterrupt(digitalPinToInterrupt(3), moveChair, CHANGE);
 ///////////////////////////////////////
+
+///SECURITY////////////////////////////
+  string password = "password" //insert password here
+
+  while (phone.getText() != password)
+  {
+    delay(100);
+  }
+///////////////////////////////////////
+
+  //Convert Arduino Blue drive numbers into arduino's bit/value range
+  throttle = map(phone.getThrottle(), 0, 99, -255, 255);
+  steering = map(phone.getSteering(), 0, 99, -255, 255); 
 }
 
 void moveChair()
 {
+    //debugging
     Serial.print("Throttle: "); Serial.print(throttle); Serial.println();
     Serial.print("Steering: "); Serial.print(steering); Serial.println();
+
+    //Reverse
     if(throttle <= 50)
     {
       throttle *= -1;
       chair.reverse(throttle, steering);
     }
+    //Forward
     else if (throttle >= 50)
+    {
       chair.forward(throttle, steering);
+    }
+    //Buffer
     else if (throttle < 50 && throttle > -50 && throttle != 0)
     {
       delay(200);
     }
+    //Chair not moving, sleep to save battery
     else
     {
       sleep_mode();
@@ -108,7 +142,6 @@ void moveChair()
 
 void loop()
 {
-  throttle = map(phone.getThrottle(), 0, 99, -255, 255);
-  steering = map(phone.getSteering(), 0, 99, -255, 255); 
+  //moveChair
   moveChair();
 }
